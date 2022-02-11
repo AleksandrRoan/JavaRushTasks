12cae669e28d4d09a52f74f983cbafe627914e7b
AleksandrRoan <alex-chalyy@yandex.ru>
2022-02-12 01:39:33 +0400

    completed new tasks in 15 level

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+
+    Solution() {
+
+    }
+
+    public Solution(String s){
+
+    }
+
+    protected Solution(int i) {
+
+    }
+
+    private Solution(double d) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
